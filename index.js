7140713f15e054bffdaf64bc2534707dd7ce5de4
jennt55 <146074617+jennt55@users.noreply.github.com>
2023-11-21 13:40:51 +0000

    files added

---
+++ index.js
@@ -0,0 +1,27 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import Search from "./Search";
+import "./Style.css";
+
+const rootElement = document.getElementById("root");
+const root = createRoot(rootElement);
+
+root.render(
+  <StrictMode>
+    <div className="searchEngine">
+      <h1>Weather Search Engine</h1>
+
+      <Search />
+    </div>
+    <p className="footer">
+      <a
+        href="https://github.com/jennt55/react-weather-app-wk4"
+        target="_blank"
+        rel="noreferrer"
+      >
+        Open Source Code
+      </a>{" "}
+      by Jenn Taft
+    </p>
+  </StrictMode>
+);

b3fe2716fc7264cb0ed459fb9ceaaf9544a6b705
jennt55 <146074617+jennt55@users.noreply.github.com>
2023-11-21 13:48:30 +0000

    updated folders

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,17 +1,27 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import Search from "./Search";
+import "./Style.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const rootElement = document.getElementById("root");
+const root = createRoot(rootElement);
+
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+  <StrictMode>
+    <div className="searchEngine">
+      <h1>Weather Search Engine</h1>
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+      <Search />
+    </div>
+    <p className="footer">
+      <a
+        href="https://github.com/jennt55/react-weather-app-wk4"
+        target="_blank"
+        rel="noreferrer"
+      >
+        Open Source Code
+      </a>{" "}
+      by Jenn Taft
+    </p>
+  </StrictMode>
+);

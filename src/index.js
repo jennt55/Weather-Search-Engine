a96f8d9ac7f3c4a96d030a731c49a28417036a75
jennt55 <146074617+jennt55@users.noreply.github.com>
2023-11-22 11:04:43 +0000

    react updated

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import Search from "./Search";
-import "./Style.css";
+import "./src/Style.css";
 
 const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
